e2bdd42160c2539aa9b296fab4e7baaa3e3a88ba
decige <ccy498@galim.com>
2025-08-07 11:29:48 +0800

    add vercel speed insights

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
  <shuo></shuo>
  <hr></hr>
  <xianshi></xianshi>
+ <SpeedInsights />
   </template>
   <script>
   import ti from './components/title.vue'
   import shuo from './components/shuo.vue'
   import xianshi from './components/xianshi.vue'
+  import { SpeedInsights } from "@vercel/speed-insights/next"
   const s={
 components:{//要引用组件在这声明
   ti,
